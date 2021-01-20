63ab136969a9d6a3a1382d013f6a4f57ecf324e7
jinchung <jin.chung17@gmail.com>
2021-01-20 12:40:00 -0700

    Support zeroEx quote

--- src/references/index.ts
+++ src/references/index.ts
@@ -26,6 +26,7 @@ export { default as supportedNativeCurrencies } from './native-currencies.json';
 export { default as shitcoins } from './shitcoins.json';
 export { ZAP_IN_ABI, ZapInAddress } from './zapper';
 
+export const RAINBOW_ADDRESS = '0x7a3d05c70581bd345fe117c06e45f9669205384f';
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const ETH_ADDRESS = 'eth';
 export const WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2';
--- src/utils/contract.js
+++ src/utils/contract.js
@@ -1,65 +1,10 @@
-import { arrayify } from '@ethersproject/bytes';
 import { MaxUint256 } from '@ethersproject/constants';
 import { Contract } from '@ethersproject/contracts';
 import { captureException } from '@sentry/react-native';
-import { fill, join } from 'lodash';
 import { loadWallet } from '../model/wallet';
 import { toHex, web3Provider } from '@rainbow-me/handlers/web3';
-import {
-  erc20ABI,
-  ethUnits,
-  smartContractMethods,
-  WETH_ADDRESS,
-  ZAP_IN_ABI,
-  ZapInAddress,
-  ZERO_ADDRESS,
-} from '@rainbow-me/references';
-import { ethereumUtils } from '@rainbow-me/utils';
-
+import { erc20ABI, ethUnits } from '@rainbow-me/references';
 import logger from 'logger';
-
-const depositEthToZap = async (
-  pairAddress,
-  minPoolTokens,
-  ethValue,
-  fromAmount = 0
-) => {
-  const swapTarget = WETH_ADDRESS;
-  const allowanceTarget = WETH_ADDRESS;
-
-  // 224 bytes = 7 (6 args and offset indicator itself) * 32 bytes
-  const offsetBytes = 224;
-  const dataBytes = 4;
-
-  const offset = ethereumUtils.padLeft(offsetBytes.toString(16), 64);
-  const dataLength = ethereumUtils.padLeft(dataBytes.toString(16), 64);
-  const depositFunction = ethereumUtils.removeHexPrefix(
-    smartContractMethods.deposit.hash
-  );
-
-  const rawSwapData = `${offset}${dataLength}${depositFunction}`;
-  const remainingPaddingLength = 64 - (rawSwapData.length % 64);
-  const remainingPadding = join(fill(Array(remainingPaddingLength), '0'), '');
-  const swapData = `${rawSwapData}${remainingPadding}`;
-  const swapDataBytes = arrayify('0x' + swapData);
-
-  const zapInContract = new Contract(ZapInAddress, ZAP_IN_ABI, web3Provider);
-  try {
-    const lpTokensBought = await zapInContract.callStatic.ZapIn(
-      ZERO_ADDRESS,
-      pairAddress,
-      fromAmount,
-      minPoolTokens,
-      allowanceTarget,
-      swapTarget,
-      swapDataBytes,
-      { value: ethValue }
-    );
-    return lpTokensBought.toString();
-  } catch (error) {
-    logger.log('Error depositing ETH to zap', error);
-  }
-};
 
 const estimateApproveWithExchange = async (owner, spender, exchange) => {
   try {
@@ -115,7 +60,6 @@ const getRawAllowance = async (owner, token, spender) => {
 
 export default {
   approve,
-  depositEthToZap,
   estimateApprove,
   getRawAllowance,
 };
