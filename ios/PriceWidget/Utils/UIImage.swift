e99cfac78806f0dfb3a9c6af29b0bfcfc4a81942
Ben Goldberg <bgoldberg@berkeley.edu>
2021-11-19 11:43:08 -0800

    Cleanup

--- ios/PriceWidget/Utils/UIImage.swift
+++ ios/PriceWidget/Utils/UIImage.swift
@@ -12,7 +12,6 @@ import UIKit
 extension UIImage {
     
     func resizeImageTo(size: CGSize) -> UIImage {
-        
         UIGraphicsBeginImageContextWithOptions(size, false, 0.0)
         self.draw(in: CGRect(origin: CGPoint.zero, size: size))
         let resizedImage = UIGraphicsGetImageFromCurrentImageContext()!
